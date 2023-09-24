2b101345b3cc9491681414832f0fc28f61b4c1b7
trisbt <tristanbott30@gmail.com>
2023-09-23 18:56:28 -0700

    docker production set vite build set

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   base: '/',
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensure this directory aligns with what Express serves
+  },
   server: {
     proxy: {
+      host: '0.0.0.0',
       '/api': 'http://localhost:4000' // adjust this according to your API routes
     }
   }

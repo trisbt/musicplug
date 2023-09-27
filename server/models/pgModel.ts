9e030068a987bf0614d6f46b4d32ebf24fa1f29e
trisbt <tristanbott30@gmail.com>
2023-09-27 11:04:55 -0700

    build v1.0.0

--- server/models/pgModel.ts
+++ server/models/pgModel.ts
@@ -1,12 +1,12 @@
 import { Pool } from 'pg';
 import dotenv from 'dotenv';
 
-// Load environment variables from the .env file
+// change host to host_dev for development
 dotenv.config();
 
 const pool = new Pool({
     user: process.env.PG_USER,
-    host: process.env.PG_HOST_DEV,
+    host: process.env.PG_HOST,
     database: process.env.PG_DATABASE,
     password: process.env.PG_PASSWORD,
     port: Number(process.env.PG_PORT)
